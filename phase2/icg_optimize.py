c3e037ddd61a3fdf56f6d61bb0a229a4530f0920
Ashish Shenoy <ashish1shenoy@gmail.com>
2021-05-04 15:06:57 +0530

    Seperate optimizations

--- phase2/icg_optimize.py
+++ phase2/icg_optimize.py
@@ -1,18 +1,20 @@
-import sys
 from functools import reduce
-from pprint import pprint
 
 const_folding_operations = {
     "ADD": "+",
     "SUB": "-",
     "MUL": "*",
-    "DIV": " /",
+    "DIV": "/",
     "MOD": "%",
     "OR": "or",
     "AND": "and",
 }
 
-variable_map = {}
+propagating_constants = {}
+
+propagating_variables = {}
+
+variable_info = {}
 
 
 def preprocess_code(code):
@@ -21,65 +23,73 @@ def preprocess_code(code):
     return quad_code
 
 
-def lookup_varmap(symbol):
-    if symbol in variable_map:
-        variable_map[symbol]["is_dead"] = False
-        return str(variable_map[symbol]["value"])
-    else:
-        return symbol
-
-
-def insert_varmap(symbol, field, value):
-    if symbol in variable_map:
-        variable_map[symbol][field] = value
-    else:
-        variable_map[symbol] = {}
-        variable_map[symbol][field] = value
-        variable_map[symbol]["is_dead"] = True
-
-
-def fold_constants(instr):
-    instr[2] = lookup_varmap(instr[2])
-    instr[3] = lookup_varmap(instr[3])
-
-    a = int(instr[2])
-    b = int(instr[3])
-
-    constant = eval(f"{a}{const_folding_operations[instr[1]]}{b}")
-    insert_varmap(instr[4], "value", constant)
-
-
-def propagate_constants(instr):
-    instr[2] = lookup_varmap(instr[2])
-    instr[3] = lookup_varmap(instr[3])
-
-    return instr
-
-
-def propagate_variable(instr):
-    instr[2] = lookup_varmap(instr[2])
-    insert_varmap(instr[3], "value", instr[2])
-    
-    return instr
-
-
-def evaluate_expression(code):
+def fold_constants(code):
     op_code = []
     for instr in code:
+        is_constant_folded = False
         if len(instr) > 1:
             if instr[1] in const_folding_operations:
                 try:
-                    fold_constants(instr)
-                except:
-                    instr = propagate_constants(instr)
+                    a = int(instr[2])
+                    b = int(instr[3])
+
+                    constant = int(eval(f"{a}{const_folding_operations[instr[1]]}{b}"))
+                    op_code.append([instr[0], "ASSIGN", str(constant), instr[4]])
+                    is_constant_folded = True
+                except ValueError:
+                    pass
+
+        if not is_constant_folded:
+            op_code.append(instr)
+
+    return op_code
+
+
+def propagate_constants(code):
+    op_code = []
+    for instr in code:
+        if len(instr) > 1:
+            if instr[1] == "ASSIGN":
+                try:
+                    constant = int(instr[2])
+                    propagating_constants[instr[3]] = constant
                     op_code.append(instr)
+                    continue
+                except ValueError:
+                    if instr[3] in propagating_constants:
+                        propagating_constants.pop(instr[3])
 
-            elif instr[1] == "ASSIGN":
-                instr = propagate_variable(instr)
-                op_code.append(instr)
+            if instr[2] in propagating_constants:
+                instr[2] = str(propagating_constants[instr[2]])
 
+            if len(instr) > 4:
+                if instr[3] in propagating_constants:
+                    instr[3] = str(propagating_constants[instr[3]])
+
+        op_code.append(instr)
+
+    return op_code
+
+
+def propagate_variables(code):
+    op_code = []
+    for instr in code:
+        if len(instr) > 1:
+            if instr[1] == "ASSIGN":
+                try:
+                    src = int(instr[2])
+                except:
+                    instr[2] = propagating_variables.get(instr[2], instr[2])
+                    try:
+                        dest = int(instr[3])
+                    except:
+                        instr[3] = propagating_variables.get(instr[3], instr[3])
+                        propagating_variables[instr[3]] = instr[2]
             else:
-                op_code.append(instr)
+                instr[2] = propagating_variables.get(instr[2], instr[2])
+                instr[3] = propagating_variables.get(instr[3], instr[3])
+
+        op_code.append(instr)
 
     return op_code
 
@@ -88,8 +98,23 @@ def eliminate_dead_code(code):
     op_code = []
 
     for instr in code:
-        if len(instr) > 1 and instr[1] == "ASSIGN":
-            if not variable_map[instr[3]]["is_dead"]:
+        if len(instr) > 1:
+            dest_var = instr[-1]
+            if dest_var not in variable_info:
+                variable_info[dest_var] = {"is_dead": True}
+            src_vars = []
+            src_vars.append(instr[2])
+            if len(instr) > 4:
+                src_vars.append(instr[3])
+
+            for var in src_vars:
+                if var in variable_info:
+                    variable_info[var]["is_dead"] = False
+
+    for instr in code:
+        if len(instr) > 1:
+            dest_var = instr[-1]
+            if not variable_info[dest_var]["is_dead"]:
                 op_code.append(instr)
 
     return op_code
@@ -102,20 +127,25 @@ def stringify_code(code):
 
 
 def main():
-    with open("icg.txt", "r") as f:
+    code : str
+    with open("icg.txt", "r+") as f:
         code = preprocess_code(f.read())
 
         optimizations = [
-            evaluate_expression,
+            fold_constants,
+            propagate_constants,
+            propagate_variables,
             eliminate_dead_code,
         ]
 
+    
         for optimize_function in optimizations:
+            with open("before_optimization.txt", "w") as bf:
+                bf.write(stringify_code(code))
             code = optimize_function(code)
-
-        code = stringify_code(code)
-        print(code)
-
+    
+    with open("icg.txt", "w") as f:
+        f.write(stringify_code(code))
 
 if __name__ == "__main__":
     main()
--- phase2/test.go
+++ phase2/test.go
@@ -3,73 +3,72 @@ package main
 import "fmt"
 
 
-// if-else statement
-var a int = 5
-if ( a > 10 ) {
-	var aa int = true || false && -40
-	var ab int = 40 + 50
-	var ac int = 5
-}  else {
-	var ba string = "Hello"
-}
+// // if-else statement
+// var a int = 5
+// if ( a > 10 ) {
+// 	var aa int = true || false && -40
+// 	var ab int = 40 + 50
+// 	var ac int = 5
+// }  else {
+// 	var ba string = "Hello"
+// }
 
-// if-else-if statement
-var b float32 = 3.5
-if ( b < 4.2 ) {
-	var ca bool = false
-} else if ( b > 6.9 ) {
-	var ba int = 69 * 420 / 666
-}
+// // if-else-if statement
+// var b float32 = 3.5
+// if ( b < 4.2 ) {
+// 	var ca bool = false
+// } else if ( b > 6.9 ) {
+// 	var ba int = 69 * 420 / 666
+// }
 
-// for loop
-var c float32 = 1.1
-for var j float32 = 4.2 ; j < 6.9; j = j + 0.1
-{
-	c = c + 0.7
-}
+// // for loop
+// var c float32 = 1.1
+// for var j float32 = 4.2 ; j < 6.9; j = j + 0.1
+// {
+// 	c = c + 0.7
+// }
 
-// Nested conditionals and loops
-var d bool = 4
-for var k int = 2 ; k < 10; k = k + 1
-{
-	if (k == 9) {
-		d = d || d
-	} else {
-		d = d && d
-	}
-}
+// // Nested conditionals and loops
+// var d bool = 4
+// for var k int = 2 ; k < 10; k = k + 1
+// {
+// 	if (k == 9) {
+// 		d = d || d
+// 	} else {
+// 		d = d && d
+// 	}
+// }
 
-// Nested loops
-var e int = 7
-for var l int = 10 ; l > 1 ; l = l - 1
-{
-	for var m int = 5 ; m > 2 ; m = m % 2
-	{
-		e = e * 3
-	}
-}
+// // Nested loops
+// var e int = 7
+// for var l int = 10 ; l > 1 ; l = l - 1
+// {
+// 	for var m int = 5 ; m > 2 ; m = m % 2
+// 	{
+// 		e = e * 3
+// 	}
+// }
 
 
-// Nested if statement
-var f int = 60
-if (f % 3) {
-	if (f % 4) {
-		if (f % 5) {
-			f = f % f
-		}
-		f = f * f
-	} else {
-		f = f / f
-	}
-}
+// // Nested if statement
+// var f int = 60
+// if (f % 3) {
+// 	if (f % 4) {
+// 		if (f % 5) {
+// 			f = f % f
+// 		}
+// 		f = f * f
+// 	} else {
+// 		f = f / f
+// 	}
+// }
 
 // Optimisation test case
 func main() int {
-	var a int = 10 + 20 + 30
-	var b int
-	var c int = b
-	var d int = a + 10
-	var e int = c * 2
-	var f int = a + 10
-	var g int = d + a
+	var a int = 10 / 5 + 8
+	var q int = 9 * 9
+	var c int = 10 - 2 + a
+	var d int = c
+	var e int = c - 5
+	var f int = d + 5
 }
